68137eba94b3a98835814be1b8ec2b0be09fb4c7
Tomáš Mühr <tomas.muhr@seznam.cz>
2023-11-23 19:20:19 +0100

    items count modal JS in progress

--- app/main/static/js/items.js
+++ app/main/static/js/items.js
@@ -26,3 +26,49 @@ form.addEventListener('submit', function(event) {
         console.log('Name field is empty.');
     }
 });
+
+// Get items count in modal
+// TODO
+document.addEventListener('DOMContentLoaded', function() {
+    var modals = document.querySelectorAll('.modal');
+    var buttons = document.querySelectorAll('.open-modal');
+    var closeButtons = document.querySelectorAll('.close');
+  
+    // Loop through each button and add a click event listener
+    buttons.forEach(function(button, index) {
+        button.addEventListener('click', function() {
+            var shopId = button.dataset.shopId; // get the shop ID from the button's data attribute
+            fetch('/get-value', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({ shop_id: shopId })
+            })
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                var modalValue = document.getElementById('modal-value');
+                modalValue.textContent = data.value; // display the value in the modal
+                modals[index].style.display = 'block'; // show the modal
+            });
+        });
+    });
+  
+    // Loop through each close button and add a click event listener
+    closeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var modal = this.closest('.modal');
+            modal.style.display = 'none'; // hide the modal
+        });
+    });
+  
+    // When the user clicks anywhere outside of the modals, close them
+    window.addEventListener('click', function(event) {
+        modals.forEach(function(modal) {
+            if (event.target == modal) {
+                modal.style.display = 'none'; // hide the modal
+            }
+        });
+    });
+});
+  
